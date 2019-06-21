cd91afd181b2b9eff025b046030cd124c5412169
Andreas Klöber <andreas.kloeber@gft.com>
2019-06-21 17:07:22 +0200

    typescript support added

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,4 @@
+// noinspection JSUnusedGlobalSymbols,JSLastCommaInObjectLiteral,JSUnusedLocalSymbols
 exports.config = {
     //
     // ====================
@@ -20,6 +21,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
+        './test/specs/**/*.ts',
         './test/specs/**/*.js'
     ],
     // Patterns to exclude.
@@ -130,7 +132,10 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'tdd',
-        timeout: 60000
+        timeout: false,
+        require: [
+            'tsconfig-paths/register'
+        ]
     },
     //
     // =====
@@ -162,8 +167,9 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that are to be run
      */
-    // before: function (capabilities, specs) {
-    // },
+    before: function (capabilities, specs) {
+        require('ts-node').register({ files: true });
+    },
     /**
      * Runs before a WebdriverIO command gets executed.
      * @param {String} commandName hook command name
@@ -239,7 +245,7 @@ exports.config = {
      * @param {Object} exitCode 0 - success, 1 - fail
      * @param {Object} config wdio configuration object
      * @param {Array.<Object>} capabilities list of capabilities details
-     * @param {<Object>} results object containing test results
+     * @param {Object} results object containing test results
      */
     // onComplete: function(exitCode, config, capabilities, results) {
     // },
@@ -250,4 +256,4 @@ exports.config = {
     */
     //onReload: function(oldSessionId, newSessionId) {
     //}
-}
+};
