5ce43b0e2dd94e4c4c17bab14d877a62e8b43ce0
Andreas Klöber <andreas.kloeber@gft.com>
2019-06-21 17:22:51 +0200

    IntelliJ project added

--- test/specs/basic.ts
+++ test/specs/basic.ts
@@ -4,8 +4,8 @@ suite('webdriver.io page', () => {
 
   test('should have the right title', () => {
     browser.url('https://webdriver.io');
-    const title = browser.getTitle();
-    assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    const title: string = browser.getTitle();
+    assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
   });
 
 });
